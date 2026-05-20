1dbdf241b3a9f2b6c87670a3ee19e9afb8648743
Zach Garrett <zach@very.coffee>
2026-05-19 20:06:06 -0400

    fix exports

--- packages/atrium/host/src/index.ts
+++ packages/atrium/host/src/index.ts
@@ -1,5 +1,9 @@
 export { createAtriumHost } from "./atrium-host.ts";
 export type { AtriumHostContext } from "./context.ts";
+export {
+  inviteRequiredFromEnv,
+  invitesPerRegistrationFromEnv,
+} from "./invites/atrium-invites.ts";
 export { assignPostAddress } from "./on-event.ts";
 export {
   authorPrincipalIdFromPostId,
@@ -8,7 +12,21 @@ export {
   type PostAddress,
 } from "./post-address-id.ts";
 export {
+  discardCellInboxRoomTickets,
+  enqueueCellInboxInline,
+} from "./relay-cell-inbox.ts";
+export {
+  popRelayInboxDrainItemsForDid,
+  type RelayInboxDrainItem,
+} from "./relay-inbox-drain.ts";
+export {
   deletePostOutboxRecord,
   listAuthorOutboxRecords,
   resolvePostById,
 } from "./resolve-post.ts";
+export {
+  authorDidFromSubscriptionSubject,
+  authorSubscriptionSubject,
+  authorTopicSubscriptionSubject,
+  parseAuthorTopicSubscriptionSubject,
+} from "./subject-keys.ts";
